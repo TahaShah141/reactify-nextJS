aa344719c4b754fa9a5f8e749745e3be9cf2f1d2
Abbas-Askari <abbasaskari797@gmail.com>
2024-05-19 01:38:09 +0500

    refactored code

--- components/pageComponents/ProjectsPage/WorkPlace/Sidebar/CodePreview/CodePreview.tsx
+++ components/pageComponents/ProjectsPage/WorkPlace/Sidebar/CodePreview/CodePreview.tsx
@@ -1,18 +1,15 @@
 'use client';
 
-import { getParentChild } from "@/lib/componentType"
 import { useAppSelector } from "@/lib/redux/hooks"
 import { selectComponents } from "@/lib/redux/store"
-import { generateCode, generateComponentCode } from "@/lib/utils"
+import { generateComponentCode } from "@/lib/utils"
+import { downloadZip } from "./codeUtils";
 
 import CodeMirror from "@uiw/react-codemirror";
 import { javascript } from "@codemirror/lang-javascript";
 import { vscodeDark } from "@uiw/codemirror-theme-vscode";
-import { useSelector, useDispatch } from "react-redux";
 import { useState } from "react";
 
-import JSzip from "jszip"
-import { cardJSX, eslintrcCJS, gitignore, indexHTML, mainJSX, packageJSON, postcssConfigJS, tailwindConfigJS, viteConfigJS, viteSVG } from "./fileConstants";
 
 
 
@@ -24,43 +21,6 @@ export const CodePreview = () => {
   const codes = tabNames.map(name => generateComponentCode(tabs[name].root, name));
   const index = tabNames.findIndex(name => name === selected);
   const code = codes[index];
-
-
-  function downlaodBlob(blob: Blob) {
-    var a = document.createElement("a");
-    document.body.appendChild(a);
-    const url = window.URL.createObjectURL(blob);
-    a.href = url;
-    a.download = "hello.zip";
-    a.click();
-    window.URL.revokeObjectURL(url);
-  }
-
-  function downloadZip() {
-    const zip = new JSzip();
-    const srcZip = zip.folder('src');
-    if (!srcZip) throw new Error("Couldn't create 'src' folder");
-    codes.forEach((code, i) => {
-      srcZip.file(`${tabNames[i]}.jsx`, code);
-    })
-    zip.file('public/vite.svg', viteSVG);
-    zip.file('src/index.css', "@tailwind base;\n@tailwind components;\n@tailwind utilities;\n");
-    zip.file('src/main.jsx', mainJSX);
-    zip.file('src/card.jsx', cardJSX);
-    zip.file('package.json', packageJSON);
-    zip.file('index.html', indexHTML);
-    zip.file('postcss.config.js', postcssConfigJS);
-    zip.file('tailwind.config.js', tailwindConfigJS);
-    zip.file('vite.config.js', viteConfigJS);
-    zip.file('.gitignore', gitignore);
-    zip.file('.eslintrc.cjs', eslintrcCJS);
-
-    zip.generateAsync({ type: 'blob' }).then(blob => {
-      downlaodBlob(blob);
-      console.log(blob)
-    })
-  }
-
   const className = `px-4 min-w-48 w-48 h-9 flex items-center justify-center border rounded-sm hover:bg-foreground/10`
 
 
@@ -75,7 +35,7 @@ export const CodePreview = () => {
             className={`${className} ${selected === name ? "text-primary bg-foreground/5" : "text-muted-foreground"}`}  >{name}.jsx</button>
         )}
         <button
-          onClick={downloadZip}
+          onClick={() => downloadZip(tabNames, codes)}
           className={`${className} text-muted-foreground`}  >Download as Zip</button>
       </div>
       <div className="  flex-1 flex items-stretch">
