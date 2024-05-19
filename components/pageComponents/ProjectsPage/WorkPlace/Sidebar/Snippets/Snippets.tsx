acc3107a246b2c5d13243fdb00288ea7e8fa1dce
Taha Shah <tahashah411@gmail.com>
2024-05-19 18:52:51 +0500

    fix sidebar snippets display

--- components/pageComponents/ProjectsPage/WorkPlace/Sidebar/Snippets/Snippets.tsx
+++ components/pageComponents/ProjectsPage/WorkPlace/Sidebar/Snippets/Snippets.tsx
@@ -4,21 +4,17 @@ import { FavoriteSnippets } from "./FavoriteSnippets"
 
 export const Snippets = () => {
   return (
-    // <Tabs defaultValue='favorites' className='w-full'>
-    //   <TabsList className="grid w-full grid-cols-2">
-    //     <TabsTrigger value='all'>All</TabsTrigger>
-    //     <TabsTrigger value='favorites'>Favorites</TabsTrigger>
-    //   </TabsList>
-    //   <TabsContent value='favorites'>
-    //     <FavoriteSnippets />
-    //   </TabsContent>
-    //   <TabsContent value='all'>
-    //     <AllSnippets />
-    //   </TabsContent>
-    // </Tabs>
-    <>
+    <Tabs defaultValue='favorites' className='w-full'>
+      <TabsList className="grid w-full grid-cols-2">
+        <TabsTrigger value='all'>All</TabsTrigger>
+        <TabsTrigger value='favorites'>Favorites</TabsTrigger>
+      </TabsList>
+      <TabsContent value='favorites'>
         <FavoriteSnippets />
+      </TabsContent>
+      <TabsContent value='all'>
         <AllSnippets />
-    </>
-      )
+      </TabsContent>
+    </Tabs>
+  )
 }
